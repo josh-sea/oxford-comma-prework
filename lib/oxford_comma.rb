ead276a1bfc48de80022f2ca0e1595b4902b7fe6
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-11-10 19:57:38 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 1 
     return array
   elsif array.length == 2
-    return array.join("and ")
+    return array.join(" and ")
   else
     array.insert(-1, "and")
     return array.join(", ")
